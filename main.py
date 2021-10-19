6117171f95a6f2e1e94d19aae2b4503bf0dca7c5
Zaharie Stefan <stefan@zaha.tech>
2021-10-19 07:56:36 +0300

    Problema 2

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+def get_if_its_present(lst, elem) -> bool:
+    for i in lst:
+        if elem == i:
+            return True
+    return False
+
+
+def run_get_if_its_present(lst):
+    elem = input("Introduceti un element: ")
+    if get_if_its_present(lst, elem) == True:
+        print("DA")
+    else:
+        print("NU")
+
+
+def test_get_if_its_present():
+    assert get_if_its_present(["abcd"], "abcd") == True
+    assert get_if_its_present([], "a") == False
+
+
 def print_menu():
     menu = """
 i. Citire lista
 l. Afisare lista
 m. Afisare meniu
 x. Iesite
+
+1. Verificare daca un element dat este in lista
 """
     print(menu)
+
+
+def run_tests():
+    test_get_if_its_present()
 
 
 def input_list():
@@ -13,8 +39,10 @@ def input_list():
     raw_input = input("Introduceti lista de elemente separate prin spatii: ")
     return raw_input.split()
 
+
 def main():
     print_menu()
+    run_tests()
     lst = []
 
     while True:
@@ -28,6 +56,13 @@ def main():
             print(lst)
         elif option == "m":
             print_menu()
+
+        elif option == "1":
+            run_get_if_its_present(lst)
+
+        else:
+            print("Optiune inexistenta!")
+            
 
 
 if __name__ == "__main__":
